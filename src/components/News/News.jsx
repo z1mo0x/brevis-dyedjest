1a10c846ed0f795a49df6d9083ff882b16efed32
z1mo0x <droweckiy@mail.ru>
2025-06-08 11:19:52 +0300

    first-download

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './News.module.css'
 import NewsItem from '../NewsItem/NewsItem';
 import { supabase } from '../../supabase'
+import { Link } from 'react-router-dom';
 
 export default function News() {
     const [data, setData] = useState([]);
@@ -31,6 +32,7 @@ export default function News() {
     return (
         <div className={styles.news}>
             <h1>Дайджест недели</h1>
+            <NavLink to="/create" className={styles.button__add}>Добавить новость</NavLink>
             <ul className={styles.news__list}>
                 {data.map((item) => (
                     <NewsItem key={item.id} id={item.id} title={item.title} type={item.type} description={item.description} user={item.user} created_at={item.created_at} />
